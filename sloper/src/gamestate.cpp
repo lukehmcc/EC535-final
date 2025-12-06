46193dff8ebd77ef1283c3207991f9de677486a7
covalent <mail@lukehmcc.com>
2025-12-06 14:10:18 -0500

    feat(sloper): add a bunch of tree variations

--- sloper/src/gamestate.cpp
+++ sloper/src/gamestate.cpp
@@ -3,7 +3,7 @@
 #include <QThread>
 #include <QTimer>
 #include "helpers.h"
-#include <iostream>
+#include <random>
 
 GameState::GameState(AnimatableImage *g,
                      AnimatableImage *t1,
@@ -52,7 +52,6 @@ void GameState::reset()
 {
     paused = false;
     animationTime = 10000; // 6s
-    animationTimer->start(animationTime);
     elapsed->restart();
     kickStartTrees();
     colTimer->start(10);
@@ -69,25 +68,43 @@ int proportionalTime(double distance1, int distance2, int time)
     return (static_cast<double>(distance2 - 1150) / (distance1 - 1150)) * time;
 }
 
+static const std::array<std::array<int, 3>, 50> combos{
+    {{-1150, -850, -450}, {-1100, -900, -500}, {-1050, -750, -350}, {-1000, -650, -250},
+     {-950, -550, -150},  {-900, -700, -300},  {-850, -500, -200},  {-800, -600, -400},
+     {-750, -450, -150},  {-700, -550, -350},  {-650, -750, -250},  {-600, -500, -300},
+     {-550, -850, -400},  {-500, -650, -200},  {-450, -750, -350},  {-400, -600, -150},
+     {-350, -700, -250},  {-300, -550, -450},  {-250, -650, -350},  {-200, -750, -400},
+     {-1150, -650, -250}, {-1100, -700, -300}, {-1050, -600, -350}, {-1000, -750, -200},
+     {-950, -500, -400},  {-900, -550, -450},  {-850, -600, -150},  {-800, -650, -350},
+     {-750, -700, -250},  {-700, -450, -300},  {-650, -500, -400},  {-600, -850, -200},
+     {-550, -750, -350},  {-500, -650, -250},  {-450, -550, -150},  {-400, -700, -400},
+     {-350, -600, -300},  {-300, -750, -200},  {-250, -850, -350},  {-200, -500, -450},
+     {-1150, -750, -350}, {-1100, -550, -250}, {-1050, -650, -200}, {-1000, -850, -400},
+     {-950, -600, -300},  {-900, -750, -150},  {-850, -700, -400},  {-800, -500, -350},
+     {-750, -550, -200},  {-700, -850, -300}}};
+
+static std::mt19937 rng(std::random_device{}());
+
 // restarts the tree animations
 void GameState::kickStartTrees()
 {
-    int t1 = proportionalTime(-1200, -850, animationTime);
-    int t2 = proportionalTime(-1200, -150, animationTime);
-    int t3 = proportionalTime(-1200, -1200, animationTime);
-    animation1 = Helpers().startAnimation(tree1, 50, -850, t1);
-    animation2 = Helpers().startAnimation(tree2, 200, -150, t2);
-    animation3 = Helpers().startAnimation(tree3, 350, -1200, t3);
-    // tree controller logic
-    // infinite while loop
+    auto [p1, p2, p3] = combos[rng() % combos.size()];
+    int t1 = proportionalTime(-1200, p1, animationTime);
+    int t2 = proportionalTime(-1200, p2, animationTime);
+    int t3 = proportionalTime(-1200, p3, animationTime);
+    animation1 = Helpers().startAnimation(tree1, 50, p1, t1);
+    animation2 = Helpers().startAnimation(tree2, 200, p2, t2);
+    animation3 = Helpers().startAnimation(tree3, 350, p3, t3);
+
     animationTimer = new QTimer();
     QObject::connect(animationTimer, &QTimer::timeout, [this]() {
-        int t1 = proportionalTime(-1200, -850, animationTime);
-        int t2 = proportionalTime(-1200, 0, animationTime);
-        int t3 = proportionalTime(-1200, -1200, animationTime);
-        animation1 = Helpers().startAnimation(tree1, 50, -850, t1);
-        animation2 = Helpers().startAnimation(tree2, 200, 0, t2);
-        animation3 = Helpers().startAnimation(tree3, 350, -1200, t3);
+        auto [p1, p2, p3] = combos[rng() % combos.size()];
+        int t1 = proportionalTime(-1200, p1, animationTime);
+        int t2 = proportionalTime(-1200, p2, animationTime);
+        int t3 = proportionalTime(-1200, p3, animationTime);
+        animation1 = Helpers().startAnimation(tree1, 50, p1, t1);
+        animation2 = Helpers().startAnimation(tree2, 200, p2, t2);
+        animation3 = Helpers().startAnimation(tree3, 350, p3, t3);
         animationTime = std::max(animationTime - 100, 100);
         animationTimer->setInterval(animationTime);
     });
